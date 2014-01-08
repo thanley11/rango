4008aecec7c951fe768457f03f27346e5b29afec
Tom Hanley <thanley11@gmail.com>
2014-01-07 19:12:44 -0500

    Fixed bing search function

--- rango/bing_search.py
+++ rango/bing_search.py
@@ -2,67 +2,67 @@ import json
 import urllib, urllib2
 
 def run_query(search_terms):
-	root_url = 'https://api.datamarket.azure.com/Bing/Search/v1/Composite'
-	source = 'Web'
-	
-	results_per_page = 10
-	offset = 0
-	
-	query = "'{0}'".format(search_terms)
-	query = urllib.quote(query)
+    root_url = 'https://api.datamarket.azure.com/Bing/Search/'
+    source = 'Web'
+    
+    results_per_page = 10
+    offset = 0
+    
+    query = "'{0}'".format(search_terms)
+    query = urllib.quote(query)
 
-	
-	search_url = "{0}{1}?$format=json&$top={2}&$skip={3}&Query={4}".format(
-		root_url,
-		source,
-		results_per_page,
-		offset,
-		query)
-	
-	
-	
-	username = ''
-	bing_api_key = 'VQMSe43qvOZ77oRcVJ10sMINEhqhonI/pemaoz6Vy54'
-	
-	password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-	password_mgr.add_password(None, search_url, username, bing_api_key)
-	
-	results = []
-	
-	try:
-		handler = urllib2.HTTPBasicAuthHandler(password_mgr)
-		opener = urllib2.build_opener(handler)
-		urllib2.install_opener(opener)
-		
-		response = urllib2.urlopen(search_url).read()
-		
-		json_response = json.loads(response)
-		
-		
-		for result in json_response['d']['hello']:
-			results.append({
-				'title': result['Title'],
-				'link': result['Url'],
-				'summary' : result['Description']})
-	
-	except urllib2.URLError, e:
-		print "Error when querying the Bing API: ", e
+    
+    search_url = "{0}{1}?$format=json&$top={2}&$skip={3}&Query={4}".format(
+        root_url,
+        source,
+        results_per_page,
+        offset,
+        query)
+    
+    
+    
+    username = ''
+    bing_api_key = 'VQMSe43qvOZ77oRcVJ10sMINEhqhonI/pemaoz6Vy54'
+    
+    password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+    password_mgr.add_password(None, search_url, username, bing_api_key)
+    
+    results = []
+    
+    try:
+        handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+        opener = urllib2.build_opener(handler)
+        urllib2.install_opener(opener)
+        
+        response = urllib2.urlopen(search_url).read()
+        
+        json_response = json.loads(response)
+        
+        
+        for result in json_response['d']['results']:
+            results.append({
+                'title': result['Title'],
+                'link': result['Url'],
+                'summary' : result['Description']})
+    
+    except urllib2.URLError, e:
+        print "Error when querying the Bing API: ", e
 
-	return results
+    return results
 
 def main():
-	query = raw_input("Please enter a query")
-	results = run_query(query)
-	
-	rank = 1
-	
-	for result in results:
-		print "Rank {0}".format(rank)
-		print result['title']
-		print result['link']
-		print
-		
-		rank += 1
-		
+    query = raw_input("Please enter a query")
+    results = run_query(query)
+    
+    rank = 1
+    
+    for result in results:
+        print "Rank {0}".format(rank)
+        print result['title']
+        print result['link']
+        print
+        
+        rank += 1
+        
 if __name__=='__main__':
-	main()
+    main()
