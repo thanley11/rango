4feeb5194913510d32ea8292a474c28659080456
Tom Hanley <thanley11@gmail.com>
2014-01-23 13:06:46 -0500

    Added PageAdmin list_display to show more Page information

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
 from rango.models import Category, Page, UserProfile
 
+class PageAdmin(admin.ModelAdmin):
+
+    list_display = ("category","title","url","views")
+    
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
 admin.site.register(UserProfile)
+
--- rango/models.py
+++ rango/models.py
@@ -25,3 +25,4 @@ class UserProfile(models.Model):
     
     def __unicode__(self):
         return self.user.username
+        
